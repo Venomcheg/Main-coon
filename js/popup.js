ee1898253745aa19b2aa643dfeff7ae87b6a5f5a
Nikita Fidyk <niknamenik@mail.ru>
2020-10-20 21:39:12 +0700

    Main-coon fix

--- js/popup.js
+++
@@ -1,7 +0,0 @@
-let user_icon = document.querySelector(".popup-number__btn");
-user_icon.addEventListener("click", function (e) {
-	let user_menu_btn = document.querySelector(".popup-number__btn");
-	let user_menu = document.querySelector(".popup-number__wrap-text");
-	user_menu.classList.toggle("_active");
-	user_menu_btn.classList.toggle("_active");
-});
